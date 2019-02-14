8249ceb1bc657cd4afdbef8f6cbfd17147918488
Ajitkaur Saini <ajitkaursaini@Ajitkaurs-MacBook-Pro.local>
2019-02-14 09:41:44 -0500

    First commit

---
+++ src/app/mock-skills.ts
@@ -0,0 +1,14 @@
+import { Skill } from './skill';
+
+export const SKILLS: Skill[] = [
+	  { id: 1, name: 'JavaScript'},
+	  { id: 2, name: 'Angular' },
+	  { id: 3, name: 'Node.js' },
+	  { id: 4, name: 'MySQL' },
+	  { id: 5, name: 'Git' },
+	  { id: 6, name: 'React' },
+	  { id: 7, name: 'Java' },
+	  { id: 8, name: 'RestAPI' },
+	  { id: 9, name: 'Python' },
+	  { id: 10, name: 'HTML' }
+];
