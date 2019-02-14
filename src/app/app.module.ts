7b2121757ee4d911fb6432393354fd28262e59d2
Ajitkaur Saini <ajitkaursaini@Ajitkaurs-MacBook-Pro.local>
2019-02-14 13:04:09 -0500

    Added my-project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SkillsComponent } from './skills/skills.component';
+import { SkillsComponent } from './my-skills/skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SkillsComponent,
+    MyProjectsComponent,
   ],
   imports: [
     BrowserModule,
