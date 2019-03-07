51f30898c0d854a5da361ab6d0928b7621b421df
gillbalraj <balrajgill18@gmail.com>
2019-03-07 17:28:55 -0500

    added new project and changed font to san-sarif

--- src/app/my-tech-list/mock-skills.ts
+++ src/app/my-tech-list/mock-skills.ts
@@ -1,14 +1,14 @@
 import { Skill } from './my-tech-list.component';
 
 export const SKILLS: Skill[] = [
-	  { id: 1, name: 'JavaScript'},
-	  { id: 2, name: 'Angular' },
-	  { id: 3, name: 'Node.js' },
-	  { id: 4, name: 'MySQL' },
-	  { id: 5, name: 'Git' },
-	  { id: 6, name: 'React' },
-	  { id: 7, name: 'Java' },
-	  { id: 8, name: 'RestAPI' },
-	  { id: 9, name: 'Python' },
-	  { id: 10, name: 'HTML' }
+	  { id: 1, name: 'AWS Cloud'},
+	  { id: 2, name: 'REST API' },
+	  { id: 3, name: 'MVC Arch' },
+	  { id: 4, name: 'Bootstrap' },
+	  { id: 5, name: 'Angular Material' },
+	  { id: 6, name: 'Google Analytics' },
+	  { id: 7, name: 'Block-chain' },
+	  { id: 8, name: 'Python' },
+	  { id: 9, name: 'PHP' },
+	  { id: 10, name: 'Wordpress' }
 ];
